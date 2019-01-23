e8080e1f57bc13e11cd83afa00bf48592f41c27f
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2019-01-23 19:48:45 +0800

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,28 +1,44 @@
-#include "Complex.h"
-#include <iostream>
+/**
+* @file Source.cpp
+*
+* @brief This code tests Operator Overloading on Complex numbers.
+*
+* @author Saif Ullah Ijaz
+*
+* TODO: Saif 190123: possible division problem while cascading.
+* Use brackets carefully. For details see KLUDGE on line 183 in "Complex.h"
+*
+*/
 
+// SYSTEM INCLUDES
+#include<iostream>
 using namespace std;
 
+// LOCAL INCLUDES
+#include "Complex.h"
+
+// function main begins program execution
 void main()
 {
-	Complex c1(23,8),c2 = "5+4i", c3("-5 + 0 j");
+	Complex c1(23, 8), c2 = (string)"5+4i", c3("-5 + 0 j");
 	
-	Complex t0 = (c1 / 8. / c2 / c3);
-	Complex t1 = 8. / (c1 / c3) / c2;
-	Complex t2 = c2 / 8. / (c1 / c3);
-	Complex t3 = c2 / 8. / (c3) / c1;
-	Complex t4 = (c3) / (c1) * 8. / (c2);
-	Complex t5 = (c3) / c2 / (c1) / 8.;
+	
+	Complex t0 = (c1 + 8. - c2 * c3);
+	Complex t1 = 8. * (c1 + c3) - c2;
+	Complex t2 = c2 * 8. / (c1 + c3);
+	Complex t3 = c2 - 8. * (c3) / c1;
+	Complex t4 = (c3) + (c1) - 8. * (c2);
+	Complex t5 = (c3) * c2 + (c1) - 8.;
 	cout << t0 << endl;
 	cout << t1 << endl;
 	cout << t2 << endl;
 	cout << t3 << endl;
 	cout << t4 << endl;
 	cout << t5 << endl;
-	
+
 	Complex c4;
 	c4 = (Complex)"23 + 8j";
-	if (c4 != c1)
+	if (c4 != c2)
 		cout << "Not Equal" << endl;
 	else
 		cout << "Equal" << endl;
@@ -36,7 +52,7 @@ void main()
 
 	double ab = c4;
 	cout << ab << endl;
-
-
+	
 	system("pause");
 }
+// end main
